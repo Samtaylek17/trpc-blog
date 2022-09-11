aa455498e6ce8f84257a52efc0c711519ea654c4
samtaylek17 <ogunsusitemitayo99@gmail.com>
2022-09-12 00:00:32 +0100

    Node mailer and login otp

---
+++ src/pages/login.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { CreateUserInput } from '../schema/user.schema';
+import { trpc } from '../utils/trpc';
+
+function LoginPage() {
+	const { handleSubmit, register } = useForm<CreateUserInput>();
+	const [success, setSuccess] = useState(false);
+
+	const router = useRouter();
+
+	const { mutate, error } = trpc.useMutation(['users.request-otp'], {
+		onSuccess: () => {
+			setSuccess(true);
+		},
+	});
+
+	function onSubmit(values: CreateUserInput) {
+		mutate(values);
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				{error && error.message}
+				{success && <p>Check your email</p>}
+				<h1>Login</h1>
+				<input type='email' placeholder='jane.doe@example.com' {...register('email')} />
+				<br />
+				<button type='submit'>Login</button>
+			</form>
+			<Link href='/register'>Register</Link>
+		</>
+	);
+}
+
+export default LoginPage;
--- src/server/route/app.router.ts
+++ src/server/route/app.router.ts
@@ -1,6 +1,6 @@
 import { createRouter } from '../createRouter';
 import { userRouter } from './user.router';
 
-export const appRouter = createRouter().merge('users', userRouter);
+export const appRouter = createRouter().merge('users.', userRouter);
 
 export type AppRouter = typeof appRouter;
